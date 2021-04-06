c1c3b1caa254d548ee8800bd1763f6c64625352a
mdurakiewicz <marcin.durakiewicz11@gmail.com>
2021-04-06 19:30:09 +0200

    Fix crash: when proguard enabled ListItem has null value in imdbID

--- common.addons/src/main/java/com/vp/commonaddons/model/ListItem.kt
+++ common.addons/src/main/java/com/vp/commonaddons/model/ListItem.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class ListItem(@SerializedName("Title") var title: String? = null,
                     @SerializedName("Year") var year: String? = null,
-                    var imdbID: String? = null,
+                    @SerializedName("imdbID") var imdbID: String? = null,
                     @SerializedName("Poster") var poster: String? = null)
